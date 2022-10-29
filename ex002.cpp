9e429d7be292553f92624c5a316c446f44240458
lukecampardo <lucascampardo@outlook.com>
2022-10-29 20:58:23 -0300

    Exercícios feitos para praticar operações e tipos númericos utilizando a lib iostream

---
+++ ex002.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+ int main() {
+	float num1, num2;
+
+	cout << "Digite o primeiro numero: ";
+	cin >> num1;
+	cout << "Digite o segundo numero: ";
+	cin >> num2;
+
+	cout << "Soma: " << num1 + num2 << endl;
+	cout << "Subtracao: " << num1 - num2 << endl;
+	cout << "Multiplicacao: " << num1 * num2 << endl;
+	cout << "Divisao: " << num1 / num2 << endl;
+
+	return 0;
+}
