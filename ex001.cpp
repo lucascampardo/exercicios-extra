3dc9de30158b65e5bd54a2c79adf69b4fcc6cea5
lukecampardo <lucascampardo@outlook.com>
2022-10-29 14:03:05 -0300

    Exercício usando cin pela primeira vez

---
+++ ex001.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n1, n2;
+	int sum;
+
+	cout << "Insira um numero: ";
+	cin >> n1;
+	cout << "Insira outro numero: ";
+	cin >> n2;
+	
+	sum = n1 + n2;
+	cout << "Soma = " << sum << endl;
+	
+	return 0;
+}
